173be3daa984049482a473a604bad3c927e8d467
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2023-02-07 20:22:50 -0300

    feat(postcard): add tests

---
+++ src/components/PostCard/PostCard.spec.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+
+import { PostCardComponent } from ".";
+import { postCardPropsMock } from "./mock";
+
+const props = postCardPropsMock;
+
+describe("<PostCardComponent />", () => {
+  it("should render PostCard correctly", () => {
+    render(<PostCardComponent {...props} />);
+
+    expect(screen.getByAltText(props.title)).toHaveAttribute(
+      "src",
+      props.cover
+    );
+
+    expect(
+      screen.getByRole("heading", { name: props.title })
+    ).toBeInTheDocument();
+
+    expect(screen.getByText("body 1")).toBeInTheDocument();
+  });
+
+  it("should match snapshot", () => {
+    const { container } = render(<PostCardComponent {...props} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
